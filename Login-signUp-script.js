9794012c2bf00d80a4f087bec0f95ebf4a6397c0
CNKrain <157701745+CNKrain@users.noreply.github.com>
2024-02-16 14:16:55 +0800

    Add files via upload

---
+++ Login-signUp-script.js
@@ -0,0 +1,17 @@
+var sign_upBox = document.getElementById("SignUpPage");
+function openSign_upBox(){
+    sign_upBox.style.visibility = 'visible';
+}
+function hideSignUp(){
+    sign_upBox.style.visibility = 'hidden';
+}
+
+    /* Functions Open/Close Log In Page */
+    var loginPage = 
+    document.getElementById('loginPage');
+    function showLogin() {
+    loginPage.style.visibility='visible';
+    }
+    function hideLogin() {
+    loginPage.style.visibility='hidden';
+    }
